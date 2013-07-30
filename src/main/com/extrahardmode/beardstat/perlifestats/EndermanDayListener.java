5bc4fb230431c62b123f58fc96f6335e545ea328
max <di.em.ex@gmx.de>
2013-07-30 20:35:57 +0200

    Add stat for enderman kiled in daylight

---
+++ src/main/com/extrahardmode/beardstat/perlifestats/EndermanDayListener.java
@@ -0,0 +1,41 @@
+package com.extrahardmode.beardstat.perlifestats;
+
+
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+/**
+ * @author Diemex
+ */
+public class EndermanDayListener implements Listener
+{
+    private final ExtraStatsPlugin plugin;
+
+
+    public EndermanDayListener(ExtraStatsPlugin plugin)
+    {
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+
+    /**
+     * When an Enderman is killed in daylight
+     */
+    @EventHandler
+    public void onEndermanDeath(EntityDeathEvent event)
+    {
+        if (event.getEntity() instanceof Enderman)
+        {
+            Enderman enderman = (Enderman) event.getEntity();
+            if (enderman.getLocation().getBlock().getLightLevel() >= 15 && enderman.getKiller() != null)
+            {
+                Player player = (Player) enderman.getKiller();
+                plugin.getStat(player.getName(), player.getWorld().getName(), "daylightkills", event.getEntityType().name().toLowerCase().replace("_", "")).incrementStat(1);
+            }
+        }
+    }
+}
