14555cd53a09bd264cf766a816221377878afe6c
max <di.em.ex@gmx.de>
2013-07-30 16:25:42 +0200

    Update Plugin for new Listeners

--- src/main/com/extrahardmode/beardstat/perlifestats/ExtraStatsPlugin.java
+++ src/main/com/extrahardmode/beardstat/perlifestats/ExtraStatsPlugin.java
@@ -5,14 +5,12 @@ import com.tehbeard.BeardStat.BeardStat;
 import com.tehbeard.BeardStat.containers.EntityStatBlob;
 import com.tehbeard.BeardStat.containers.IStat;
 import com.tehbeard.BeardStat.containers.PlayerStatManager;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Monster;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -29,50 +27,16 @@ public class ExtraStatsPlugin extends JavaPlugin implements Listener
     {
         playerStatManager = getBeardstat().getStatManager();
         getServer().getPluginManager().registerEvents(this, this);
+        new BowHitsListener(this);
+        new SheepShearListener(this);
+        new TntKillsListener(this);
     }
 
 
-    /**
-     * Extra statistic for the distance when you hit a monster/player with a bow
-     */
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    private void onBowHit(EntityDamageByEntityEvent event)
-    {
-        LivingEntity entity = event.getEntity() instanceof LivingEntity ? (LivingEntity) event.getEntity() : null;
-        //Only mobs, killing animals is too easy
-        if (event.getDamager() instanceof Arrow)
-        {
-            Arrow arrow = (Arrow) event.getDamager();
-            if (arrow.getShooter() instanceof Player)
-            {
-                Player player = (Player) arrow.getShooter();
-                //Slime is not a monster for some reason...
-                if (entity instanceof Monster || entity instanceof Player)
-                {
-                    //get distance between shooter and damagee
-                    int distance = (int) event.getEntity().getLocation().distance(player.getLocation());
-                    IStat currStat = getStat(player, player.getWorld().getName(), "bowhitdist", entity instanceof Player ? "player" : "monster");
-                    //Update value if new shot is further away
-                    if (distance > currStat.getValue())
-                        currStat.setValue(distance);
-
-                    //Separate stat for bowkills
-                    if (entity.getHealth() - event.getDamage() <= 0)
-                    {
-                        IStat killStat = getStat(player, player.getWorld().getName(), "bowkilldist", entity instanceof Player ? "player" : "monster");
-                        if (distance > killStat.getValue())
-                            killStat.setValue(distance);
-                    }
-                }
-            }
-        }
-    }
-
-
-    public IStat getStat(Player player, String world, String category, String statId)
+    public IStat getStat(String player, String world, String category, String statId)
     {
         //creates a new stat defaulting to 0 if not found
-        return getPlayer(player.getName()).getStat(ourDomain, world, category, statId);
+        return getPlayer(player).getStat(ourDomain, world, category, statId);
     }
 
 
